12e2c0a164222ad7d0fa0fb7c673b71f006d0be7
Maksim <kabanov.maks.business@gmail.com>
2023-05-16 20:05:00 +0300

    Project setting

---
+++ Portfolio/SPA Traning/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <header>
+          <h1>Главный заголовок!</h1>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default App;
